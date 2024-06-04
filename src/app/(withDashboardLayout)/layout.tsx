788ab2dc01c04e359da25bd27b329f3adf58bb63
MostakemHossain <mostakememon123@gmail.com>
2024-06-04 20:45:31 +0600

    dashboard layout implement

---
+++ src/app/(withDashboardLayout)/layout.tsx
@@ -0,0 +1,8 @@
+import DashBoardDrawer from "@/components/Dashboard/DashboardDrawer/DashBoardDrawer";
+import React from "react";
+
+const DashBoardLayout = ({ children }: { children: React.ReactNode }) => {
+  return <DashBoardDrawer>{children}</DashBoardDrawer>;
+};
+
+export default DashBoardLayout;
